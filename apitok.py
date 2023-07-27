001cd0913a4879c987fc84d7f80448005998230d
thanmayee-29 <92224593+thanmayee-29@users.noreply.github.com>
2023-07-27 16:47:50 +0530

    Add files via upload

---
+++ apitok.py
@@ -0,0 +1,3 @@
+token = 'MTA3NjMzMzY3MzgyMzA4ODc3MQ.GM_nrf.8yq4BhwNXDVY9CLGoS1bGaXDfSCyS-_1SmPFA4'
+jokeapikey = "30390f1eefmshe44e32e770f9668p128297jsn5947fc24c230"
+# jokeapihost = "joke3.p.rapidapi.com"
